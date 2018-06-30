45f28f610ecb7d6cd4d2e5cbf969ea3f93890142
Ewan Valentine <ewan.valentine89@gmail.com>
2018-06-30 12:26:16 +0100

    ADDED smart contracts

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Bloq
 
 Bloq is a Blockchain framework written in Go. Largely for my own understanding, probably not for production use. 
+
+## Use
+
+## Smart Contracts
+
+```golang
+// Dummy code, but you get the gist
+func handlerFunc(block Block) error {
+    d := map[string]string{}
+    if err := json.Marshal(block.Data, &d); err != nil {
+        return err
+    }
+    return makePayment(d["user_a"])
+}
+
+func main() {
+    ... 
+    bloq.AddContract("name:=:Ewan", handlerFunc)
+    bloq.AddContract("users:>:5", handlerFunc)
+    ...
+}
+```
--- pkg/blockchain/blockchain_test.go
+++ pkg/blockchain/blockchain_test.go
@@ -1,6 +1,7 @@
 package blockchain
 
 import (
+	"errors"
 	"testing"
 	"time"
 
@@ -21,9 +22,66 @@ func setUp(t *testing.T) *Blockchain {
 
 func TestBlockchainEntry(t *testing.T) {
 	bloq := setUp(t)
-	test := []byte("test")
+	test := []byte(`{ "test": "world" }`)
 	newBlock, err := bloq.Append(test)
 	assert.NoError(t, err)
 	assert.Equal(t, newBlock.Data, test)
 	assert.Equal(t, 2, len(bloq.GetBlocks()))
+}
+
+func TestSmartContracts(t *testing.T) {
+
+	golden := map[string][]byte{
+		"users:>:5":   []byte(`{ "users": 6 }`),
+		"name:=:Ewan": []byte(`{ "name": "Ewan" }`),
+	}
+
+	notGolden := map[string][]byte{
+		"users:>:5":   []byte(`{ "users": 5 }`),
+		"name:=:Test": []byte(`{ "name": "not Test" }`),
+	}
+
+	badContracts := map[string]error{
+		"invalid-contract":    errors.New("Invalid contract"),
+		"invalid:??:operator": errors.New("Invalid operator"),
+	}
+
+	for key, val := range golden {
+		called := false
+		handler := func([]byte) error {
+			called = true
+			return nil
+		}
+
+		bloq := setUp(t)
+		bloq.AddContract(key, handler)
+		bloq.Append(val)
+		assert.Equal(t, true, called)
+	}
+
+	for key, val := range notGolden {
+		called := false
+		handler := func([]byte) error {
+			called = true
+			return nil
+		}
+
+		bloq := setUp(t)
+		bloq.AddContract(key, handler)
+		bloq.Append(val)
+		assert.NotEqual(t, true, called)
+	}
+
+	for key, val := range badContracts {
+		called := false
+		handler := func([]byte) error {
+			called = true
+			return nil
+		}
+
+		bloq := setUp(t)
+		err := bloq.AddContract(key, handler)
+		assert.NotEqual(t, true, called)
+		assert.Error(t, err, val.Error())
+	}
 }
